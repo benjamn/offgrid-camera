b59b8d8ea9d02706a556ef97f0e125cc86f89921
Ben Newman <bn@cs.stanford.edu>
2014-07-17 11:20:40 -0400

    Move testing code out of index.js.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 var offgrid = require('./build/Release/offgrid');
 
-setTimeout(function() {
-    offgrid.capture(function(image, w, h) {
-        console.log(w, h);
-        console.log(image(0, 0));
-        console.log(image(2000, 2000));
-    });
-}, 5000);
+// Selectively re-export and/or wrap offgrid methods.
+exports.capture = offgrid.capture;
